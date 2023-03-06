9b3fa9d8f9364d87f57380bf247d6447fb9290cb
NeevyBoy <127065730+NeevyBoy@users.noreply.github.com>
2023-03-05 17:47:08 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var ball;
+function setup() {
+  createCanvas(1000,1000);
+ ball=createSprite(200,200,100,100);
+ ball.shapeColor="blue"
+}
+
+function draw() 
+{
+  background("black");
+drawSprites();
+}
+
+
+
+
